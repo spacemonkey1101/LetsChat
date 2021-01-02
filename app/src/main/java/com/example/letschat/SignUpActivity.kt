c522284a17ef91caa33d577880a208021c73bd5d
spacemonkey1101 <ritambh54@gmail.com>
2021-01-02 23:20:48 +0530

    added color

---
+++ app/src/main/java/com/example/letschat/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.example.letschat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
