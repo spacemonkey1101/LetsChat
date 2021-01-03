bee128c13f155e50dba2b6fda6a90ce9b064b8b5
spacemonkey1101 <ritambh54@gmail.com>
2021-01-03 12:31:00 +0530

    making some critical changes in logic

--- app/src/main/java/com/example/letschat/SignUpActivity.kt
+++ app/src/main/java/com/example/letschat/SignUpActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -23,10 +24,10 @@ class SignUpActivity : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
 
         btn_sign_up.setOnClickListener {
-            var userName = edit_text_name.text.toString()
-            var email = edit_text_email.text.toString()
-            var password = edit_text_password.toString()
-            var confirmpassword = edit_text_confirm_password.text.toString()
+            val userName = edit_text_name.text.toString()
+            val email = edit_text_email.text.toString()
+            val password = edit_text_password.text.toString()
+            val confirmpassword = edit_text_confirm_password.text.toString()
 
             if (TextUtils.isEmpty(userName)) {
                 Toast.makeText(applicationContext,"username is required",Toast.LENGTH_SHORT).show()
@@ -37,6 +38,9 @@ class SignUpActivity : AppCompatActivity() {
             if (TextUtils.isEmpty(password)) {
                 Toast.makeText(applicationContext,"password is required",Toast.LENGTH_SHORT).show()
             }
+            if (password.length < 6) {
+                Toast.makeText(applicationContext,"password should be 6 characters at least",Toast.LENGTH_SHORT).show()
+            }
             if (TextUtils.isEmpty(confirmpassword)) {
                 Toast.makeText(applicationContext,"please confirm your password",Toast.LENGTH_SHORT).show()
             }
@@ -44,29 +48,33 @@ class SignUpActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext,"passwords do not match",Toast.LENGTH_SHORT).show()
             }
             registerUser(userName,email,password)
+
         }
     }
     //register user function
     private fun registerUser(userName:String,email:String , password:String){
-        auth.createUserWithEmailAndPassword(email,password)
-            .addOnCompleteListener(this){
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this){
                 //if task is successful add user in database
+
                 if (it.isSuccessful) {
-                    var user: FirebaseUser? = auth.currentUser
-                    var userId: String? = user?.uid
+                    Log.i("FIRE", it.isSuccessful.toString())
+
+                    val user: FirebaseUser? = auth.currentUser
+                    val userId: String = user!!.uid
 
                     databaseReference = FirebaseDatabase.getInstance()
-                        .getReference("Users").child(userId.toString())
+                        .getReference("Users").child(userId)
 
-                    var hashMap:HashMap<String,String> = HashMap()
-                    hashMap.put("userId",userId.toString())
-                    hashMap.put("userName",userName)
-                    hashMap.put("profileImage","")
+                    val hashMap: HashMap<String, String> = HashMap()
+                    hashMap.put("userId", userId)
+                    hashMap.put("userName", userName)
+                    hashMap.put("profileImage", "")
 
-                    databaseReference.setValue(hashMap).addOnCanceledListener(this){
-                        if (it.isSuccessful){
+                    databaseReference.setValue(hashMap).addOnCompleteListener {
+                        if (it.isSuccessful) {
+
                             //open next activity
-                            var intent = Intent(this@SignUpActivity,HomeActivity::class.java)
+                            val intent = Intent(this@SignUpActivity, HomeActivity::class.java)
                             startActivity(intent)
                         }
 
