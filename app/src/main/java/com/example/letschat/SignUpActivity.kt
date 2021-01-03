f1d36318eba189a80278a51a0def19729271849a
spacemonkey1101 <ritambh54@gmail.com>
2021-01-03 08:51:44 +0530

    getting username ,password and email from the fields

--- app/src/main/java/com/example/letschat/SignUpActivity.kt
+++ app/src/main/java/com/example/letschat/SignUpActivity.kt
@@ -3,10 +3,13 @@ package com.example.letschat
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -18,6 +21,30 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(R.layout.activity_sign_up)
 
         auth = FirebaseAuth.getInstance()
+
+        btn_sign_up.setOnClickListener {
+            var userName = edit_text_name.text.toString()
+            var email = edit_text_email.text.toString()
+            var password = edit_text_password.toString()
+            var confirmpassword = edit_text_confirm_password.text.toString()
+
+            if (TextUtils.isEmpty(userName)) {
+                Toast.makeText(applicationContext,"username is required",Toast.LENGTH_SHORT).show()
+            }
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(applicationContext,"email is required",Toast.LENGTH_SHORT).show()
+            }
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(applicationContext,"password is required",Toast.LENGTH_SHORT).show()
+            }
+            if (TextUtils.isEmpty(confirmpassword)) {
+                Toast.makeText(applicationContext,"please confirm your password",Toast.LENGTH_SHORT).show()
+            }
+            if(! password.equals(confirmpassword)) {
+                Toast.makeText(applicationContext,"passwords do not match",Toast.LENGTH_SHORT).show()
+            }
+            registerUser(userName,email,password)
+        }
     }
     //register user function
     private fun registerUser(userName:String,email:String , password:String){
