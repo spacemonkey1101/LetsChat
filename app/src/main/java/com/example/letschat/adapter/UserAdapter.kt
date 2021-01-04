5f23bd3fc9dec082d1fd7be2a732b0f9fcef3df1
Ritam Bhattacharyya <ritambh54@gmail.com>
2021-01-04 10:06:53 +0530

    finishing recycler view adapter

--- app/src/main/java/com/example/letschat/adapter/UserAdapter.kt
+++ app/src/main/java/com/example/letschat/adapter/UserAdapter.kt
@@ -1,25 +1,40 @@
 package com.example.letschat.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.letschat.R
 import com.example.letschat.model.User
 
 class UserAdapter(private val context:Context, private val userList:ArrayList<User>)
-    : RecyclerView.Adapter<UserAdapter.ViewHolder> {
+    : RecyclerView.Adapter<UserAdapter.ViewHolder>() {
 
-class ViewHolder(view: View):RecyclerView.ViewHolder(view){}
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val view= LayoutInflater.from(parent.context).inflate(R.layout.item_user,parent,false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val user = userList[position]
+        holder.textUserName.text = user.userName
+        Glide.with(context).load(user.userImage).into(holder.userImage)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return userList.size
+    }
+    class ViewHolder(view: View):RecyclerView.ViewHolder(view){
+        val textUserName = view.findViewById<TextView>(R.id.userName)
+        val textTemp = view.findViewById<TextView>(R.id.temp)
+        val userImage = view.findViewById<ImageView>(R.id.user_image)
+
     }
 }
