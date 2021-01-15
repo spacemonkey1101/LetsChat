799d0f6e00619d0e312538841f6804dc93bbbc50
Ritam Bhattacharyya <ritambh54@gmail.com>
2021-01-15 21:07:00 +0530

    add upload image function

--- app/src/main/java/com/example/letschat/activity/ProfileActivity.kt
+++ app/src/main/java/com/example/letschat/activity/ProfileActivity.kt
@@ -1,5 +1,7 @@
 package com.example.letschat.activity
 
+import android.app.Dialog
+import android.app.ProgressDialog
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
@@ -10,19 +12,29 @@ import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.example.letschat.R
 import com.example.letschat.model.User
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.OnProgressListener
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
 import kotlinx.android.synthetic.main.activity_profile.*
 import kotlinx.android.synthetic.main.activity_users.*
 import kotlinx.android.synthetic.main.activity_users.imgBack
 import java.io.IOException
+import java.util.*
 
 class ProfileActivity : AppCompatActivity() {
 
     private lateinit var firebaseUser: FirebaseUser
     private lateinit var databaseReference: DatabaseReference
     private var filePath: Uri? = null
+    private lateinit var storage:FirebaseStorage
+    private lateinit var storageRef:StorageReference
+
 
     private final val PICK_IMAGE_REQUEST:Int = 2021
 
@@ -35,6 +47,10 @@ class ProfileActivity : AppCompatActivity() {
         databaseReference = FirebaseDatabase.getInstance()
             .getReference("Users")
             .child(firebaseUser.uid)
+
+        storage = FirebaseStorage.getInstance()
+        storageRef = storage.reference
+
 
 
             databaseReference.addValueEventListener(object : ValueEventListener{
@@ -79,6 +95,36 @@ class ProfileActivity : AppCompatActivity() {
             } catch (e:IOException) {
                 e.printStackTrace()
             }
+        }
+    }
+
+    private fun uploadImage() {
+        if(filePath != null) {
+            val progressDialog: ProgressDialog = ProgressDialog(this)
+            progressDialog.setTitle("Uploading...")
+            progressDialog.show()
+
+            val ref:StorageReference = storageRef.child("image/" + UUID.randomUUID().toString())
+            ref.putFile(filePath!!).addOnSuccessListener {
+                OnSuccessListener<UploadTask.TaskSnapshot> {
+                    progressDialog.dismiss()
+                    Toast.makeText(applicationContext, "Uploaded", Toast.LENGTH_SHORT).show()
+                }
+            }
+                .addOnFailureListener{
+                    OnFailureListener{
+                        progressDialog.dismiss()
+                        Toast.makeText(applicationContext, "Failed " + it.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+                .addOnProgressListener {
+                    OnProgressListener<UploadTask.TaskSnapshot>{
+                        var progress:Double = (100.0 * it.bytesTransferred / it.totalByteCount)
+                        progressDialog.setMessage("Uploaded " + progress.toInt() + "%")
+
+
+                    }
+                }
         }
     }
 }
